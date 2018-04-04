0466083d51550c91609ffa96335ff0ba09797362
Andrew <a.emartin187@gmail.com>
2018-04-04 16:03:59 -0400

    commit change

--- src/app/patient-request-info/patient-request-info.component.ts
+++ src/app/patient-request-info/patient-request-info.component.ts
@@ -8,9 +8,15 @@ import { Patient } from '../model/patient';
   styleUrls: ['./patient-request-info.component.css']
 })
 export class PatientRequestInfoComponent implements OnInit {
+<<<<<<< HEAD
   @Input() patient: Patient;
   patients: Patient[];
   constructor(private patientService: PatientService) { }
+=======
+  @Input() patient?: Patient;
+
+  constructor() { }
+>>>>>>> cc14ec8696b46dbd9c0b8fc43043fd7f874a3122
 
   ngOnInit() {
     this.getPatients();
